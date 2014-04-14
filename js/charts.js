7494123bf0de085dea17ee193b6f7fb9183f221b
Matthew Tompsett <matthewbpt at gmail dot com>
2014-04-14 21:12:51 +0100

    Pie chart!!

--- js/charts.js
+++ js/charts.js
@@ -2,50 +2,6 @@
  * Created by matthew on 13/04/14.
  */
 
-var lineGraph = function(node,data,colour) {
-
-    // getting max x and y for scaling
-    var xMax = d3.max(data, function (d) { return +d.x; });
-    var yMax = d3.max(data, function (d) { return +d.y; });
-
-    // creating axis scales
-    var xAxisScale = d3.scale.linear().domain([0,xMax]).range([0,350]);
-    var yAxisScale = d3.scale.linear().domain([0,yMax+1]).range([200,0]);
-
-    // line function to create line from data
-    var lineFunction = d3.svg.line()
-        //apply  x-axis scale
-        .x(function (d){ return xAxisScale(d.x); })
-        //apply y-axis scale
-        .y(function (d){ return yAxisScale(d.y); })
-        //monotone type line
-        .interpolate("monotone");
-
-    var svgContainer = node.append("svg")
-        .attr("width", 350)
-        .attr("height", 200);
-
-    //create and add axes
-    var xAxis = d3.svg.axis().scale(xAxisScale);
-    var yAxis = d3.svg.axis().scale(yAxisScale).ticks(4).orient("left");
-
-    svgContainer.append("g")
-        .attr("transform", "translate(0,199)")
-        .call(xAxis);
-    svgContainer.append("g")
-        .attr("transform", "translate(1,0)")
-        .call(yAxis);
-
-    //add path to svg container
-    svgContainer.append("path")
-        .attr("d", lineFunction(data))
-        .attr("stroke", colour)
-        .attr("stroke-width", 2)
-        .attr("fill", "none");
-
-    return svgContainer;
-};
-
 var horizontalColumnChart = function(node,data){
     var height = 200, width = 350;
     // create div of class columnchart
@@ -78,9 +34,68 @@ var horizontalColumnChart = function(node,data){
     return svgContainer;
 };
 
+var lineChart = function(node,data){
+    // set chart dimensions
+    var margin = {top: 10 , right: 0, bottom: 20, left: 25},
+        width = 350 - margin.left - margin.right ,
+        height = 200 - margin.top - margin.bottom;
+
+    // create vertical scale
+    var max = d3.max(data, function(d) { return +d.value; });
+    var y = d3.scale.linear().domain([0,max]).range([height,0]);
+
+    //create horizontal scale
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1)
+        .domain(data.map(function(d) { return d.category; }));
+
+    //create axis objects
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
+
+    var lineFunction = d3.svg.line()
+        //apply  x-axis scale
+        .x(function (d){ return x(d.category); })
+        //apply y-axis scale
+        .y(function (d){ return y(d.value); })
+        //monotone type line
+        .interpolate("monotone");
+
+    // create container object for chart
+    var svgContainer = node.append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // add axes to contained object
+    svgContainer.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+    svgContainer.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+    // add bars to chart
+    svgContainer.append("path")
+        //.attr("class", "line")
+        .attr("d", lineFunction(data))
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1)
+        .attr("fill", "none");
+
+    return svgContainer;
+};
+
 var verticalColumnChart = function(node,data){
     // set chart dimensions
-    var margin = {top: 0 , right: 0, bottom: 20, left: 20},
+    var margin = {top: 10 , right: 0, bottom: 20, left: 25},
         width = 350 - margin.left - margin.right ,
         height = 200 - margin.top - margin.bottom;
 
@@ -132,6 +147,52 @@ var verticalColumnChart = function(node,data){
     return svgContainer;
 };
 
+var  pieChart = function(node,data){
+    // set dimensions
+    var width = 350,
+        height = 200,
+        radius = 100,
+        color = d3.scale.category20c();
+
+    var svgContainer = node.append("svg")
+        .data([data])
+        .attr("width", width)
+        .attr("height", height)
+        .append("svg:g")
+        .attr("transform", "translate(" + width/2 + "," + radius + ")");
+
+    var arc = d3.svg
+        .arc()
+        .outerRadius(radius);
+
+    var pie = d3.layout
+        .pie()
+        .value(function(d) { return d.value; });
+
+    var arcs = svgContainer.selectAll("g.slice")
+        .data(pie)
+        .enter()
+        .append("svg:g")
+        .attr("class","slice");
+
+    arcs.append("svg:path")
+        .attr("fill", function(d,i){ return color(i); })
+        .attr("d", arc);
+
+    arcs.append("svg:text")                                     //add a label to each slice
+        .attr("transform", function(d) {                    //set the label's origin to the center of the arc
+            //we have to make sure to set these before calling arc.centroid
+            d.innerRadius = 0;
+            d.outerRadius = radius;
+            return "translate(" + arc.centroid(d) + ")";        //this gives us a pair of coordinates like [50, 50]
+        })
+        .attr("text-anchor", "middle")                          //center the text on it's origin
+        .text(function(d, i) { return data[i].label; });
+
+    return svgContainer;
+
+};
+/*
 var lineData1 = [ { "x": 1,   "y": 5},  { "x": 20,  "y": 20},
     { "x": 40,  "y": 5},  { "x": 60,  "y": 35},
     { "x": 80,  "y": 15},  { "x": 100,  "y": 45},
@@ -143,23 +204,6 @@ var lineData3 = [ { "x": 1,   "y": 100},  { "x": 20,  "y": 210},
     { "x": 80,  "y": 100},  { "x": 100,  "y": 75},
     { "x": 120, "y": 110},  { "x": 140,  "y": 129},
     { "x": 160, "y": 156},  { "x": 180,  "y": 140}];
+*/
 
-var chartData = [10,20,30,40,30,10,34,98,23,34]
 
-var moreData = [{ category: "Jan", value: 10},
-    { category: "Feb", value: 20},
-    { category: "Mar", value: 30},
-    { category: "Apr", value: 40},
-    { category: "May", value: 30},
-    { category: "Jun", value: 10},
-    { category: "Jul", value: 34},
-    { category: "Aug", value: 98},
-    { category: "Sep", value: 23},
-    { category: "Oct", value: 34},
-    { category: "Nov", value: 32},
-    { category: "Dec", value: 12}];
-
-lineGraph(d3.select("#graph1"),lineData1,"green");
-horizontalColumnChart(d3.select("#graph2"),chartData);
-verticalColumnChart(d3.select("#graph3"), moreData);
-// lineGraph(d3.select("#graph3"),lineData3,"blue");
